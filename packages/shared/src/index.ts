863d0cc7b62147d87f9875327c905ea0807c22ed
nogglee <dev.nogglee@gmail.com>
2026-05-22 19:21:44 +0900

    chore: ui, shared 패키지 추가

---
+++ packages/shared/src/index.ts
@@ -0,0 +1,20 @@
+export enum UserRole {
+  USER = "USER",
+  PARTNER = "PARTNER",
+  ADMIN = "ADMIN"
+}
+
+export enum ReservationStatus {
+  REQUESTED = "REQUESTED",
+  CONFIRMED = "CONFIRMED",
+  CANCELED = "CANCELED",
+  COMPLETED = "COMPLETED"
+}
+
+export enum ArtworkStatus {
+  CREATED = "CREATED",
+  IN_PROGRESS = "IN_PROGRESS",
+  READY = "READY",
+  DELIVERED = "DELIVERED"
+}
+
---
+++ packages/ui/src/index.ts
@@ -0,0 +1,2 @@
+export const uiPackageName = "@todam/ui";
+
