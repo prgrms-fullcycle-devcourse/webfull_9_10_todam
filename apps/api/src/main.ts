3c14ee9623a114adf92dd9770a441cce48b16004
nogglee <dev.nogglee@gmail.com>
2026-05-27 17:14:03 +0900

    feat(api): enable CORS from env

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,7 +5,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
     const env = createApiEnv();
+    const corsOrigins = env.CORS_ORIGINS.split(',').map((origin) => origin.trim());
     const app = await NestFactory.create(AppModule);
+
+    app.enableCors({ origin: corsOrigins, credentials: true });
 
     await app.listen(env.PORT);
 
