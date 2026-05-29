33cd5616947a38eb07247c302f391385d3d98557
nogglee <dev.nogglee@gmail.com>
2026-05-29 11:43:25 +0900

    feat: SpaceBlock 공통 컴포넌트 추가

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -3,5 +3,6 @@ export * from './FormBottomSheet';
 export * from './Logo';
 export * from './Modal';
 export * from './Section';
+export * from './SpaceBlock';
 export * from './StandardBottomSheet';
 export * from './Toast';
